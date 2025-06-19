7c93713f9618a062210466ab9a07447a9abaa3d2
Impishmouse <zloyboison@gmail.com>
2025-06-20 00:53:33 +0300

    cleared code. removed unncessary libraries.

--- src/main.cpp
+++ src/main.cpp
@@ -9,16 +9,16 @@
 #include <ArduinoJson.h> //json для аналізу інформації
 #include <Adafruit_NeoPixel.h> //neopixel для управління стрічкою
 #include <Led9x3Matrix.h> // Для показу еффектів.
-#include <WiFi.h> //для зв'язку
-#include <WiFiClientSecure.h>
-#include <HTTPClient.h>
-#include <WiFiManager.h> //Керування WiFi
-#include <NTPClient.h> //Час
-#include <HTTPUpdate.h> //Оновлення прошивки через тг бота
-#include <Wire.h> 
+#include <TixClockManager.h>
+
+#include <WiFi.h>
+#include <Preferences.h>
+#include <WiFiManager.h>
+#include <Wire.h>
+
 #include "time.h"
 #include <string> 
-#include <TixClockManager.h>
+
 
 // ============ НАЛАШТУВАННЯ ============
 char ssid[] = "StarLord_02";                  //"StarLord"; //Назва твоєї мережі WiFi
@@ -26,72 +26,49 @@ char password[] = "strongWifiPwd";              //"strongWifipwd"; //Парол�
 
 char APSsid[] = "TixClock"; //Назва точки доступу
 char APPassword[] = ""; //Пароль від точки доступу
+WiFiManager wm;
 
-
+// ====== Налаштування яскравості =======
 int brightness = 2; //Яскравість %
 bool autoBrightness = true; //Ввімкнена/вимкнена авто яскравість
 const int dayBrightness = 10; //Денна яскравість %
 const int nightBrightness = 1; //Нічна яскравість %
-
 const int day = 9; //Початок дня
 const int night = 21; //Початок ночі
 
-const long  gmtOffset_sec = 7200;
 
-
+// ===== Налаштування світлодіодів ======
 #define LED_PIN 13
 #define LED_COUNT 27
-
-
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 Led9x3Matrix ledDisplay(&strip);
 TixClockManager tixManager(&strip);
-
-DynamicJsonDocument doc(30000);
-
-WiFiClientSecure client;
-WiFiManager wm;
-WiFiUDP ntpUDP;
-
-const char* ntpServer = "ua.pool.ntp.org";
-
-NTPClient timeClient(ntpUDP, ntpServer, 7200);
-
-
-void colorWipe(int wait);
-
-
-
 String color;
 static int ledColor[25];
+void colorWipe(int wait);
 
-int arrAlarms = sizeof(ledColor) / sizeof(int);
+// ======= Налаштування часу 
+#define NTP_SERVER     "ua.pool.ntp.org"
+#define UTC_OFFSET     10800
+#define UTC_OFFSET_DST 0
+const long  gmtOffset_sec = 7200;
 
-// int red, green, blue;
-// bool startMessage = false;
-bool enable = false;
-int currencyMode = 1;
 int period = 8000;
 
 const long dateInterval = 200000;
-unsigned long lastTime, previousMillisA = 0, previousMillisB = 0, previousMillisC = 0, previousMillisD = 0, previousMillisE = 0, previousMillisG = 0, previousMillisH = 0, lastWeatherTime = 0, startTime = 0, previousMillisAutoSwitchDisplay = 0;
-
+unsigned long lastTime;
 
 static bool wifiConnected;
 
 String mode = "clock"; //Режим
 
 bool enabled = true;
-int disy = 0;
 // Перевірка останніх повідомлень
 
 const int   daylightOffset_sec = 3600;
 
 static int ledColorBlue[] =   { 1, 3, 8, 9, 10, 13, 15, 20, 21, 22, 25};
 static int ledColorYellow[] = { 0, 4, 7, 6, 11, 12, 16, 17, 19, 23, 24};
-
-
-
 
 void initWiFi() {
 
@@ -135,31 +112,6 @@ void colorWipe(int wait) {
   }
 }
 
-void initTime() {
-  // Встановлюємо початкове значення літнього часу на false
-  bool isDaylightSaving = false;
-  // Отримуємо поточну дату та час з сервера NTP
-  timeClient.begin();
-  timeClient.update();
-  String formattedTime = timeClient.getFormattedTime();
-  // Розбиваємо рядок з форматованим часом на складові
-  int day, month, year, hour, minute, second;
-  sscanf(formattedTime.c_str(), "%d-%d-%d %d:%d:%d", &year, &month, &day, &hour, &minute, &second);
-  // Перевіряємо, чи поточний місяць знаходиться в інтервалі березень-жовтень
-  if (month >= 4 && month <= 10) {
-    // Якщо так, встановлюємо літній час на true
-    isDaylightSaving = true;
-  }
-  // Встановлюємо зміщення часового поясу для врахування літнього часу
-  if (isDaylightSaving) {
-    timeClient.setTimeOffset(14400); // UTC+3 для України
-  }
-  else {
-    timeClient.setTimeOffset(10800); // UTC+2 для України
-  }
-}
-
-
 struct tm getLocalTime()
 {
   struct tm timeinfo;
@@ -182,9 +134,7 @@ void setup()
 
   initWiFi();
 
-  initTime();
-
-  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+  configTime(UTC_OFFSET, UTC_OFFSET_DST, NTP_SERVER);
 }
 
 void loop() {
@@ -210,12 +160,12 @@ void loop() {
       {
         if (autoBrightness) {
 		      //авто яскравість
-		      timeClient.update();
-		      int hour = timeClient.getHours();
+		      // TODO переробити це під отримання локального часу. 
+		      /*int hour = timeClient.getHours();
 		      bool isDay = hour >= day && hour < night;
 		      brightness = isDay ? dayBrightness : nightBrightness;
           strip.setBrightness(brightness * 2.55);
-          strip.show();
+          strip.show();*/
         }
 
         
